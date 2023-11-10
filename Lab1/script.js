eff7986c1685b48238e50aa3200086d3835cc4fd
Aidan705 <Aidanwilson705@gmail.com>
2023-11-10 17:54:39 -0500

    commit

--- Lab1/script.js
+++ Lab1/script.js
@@ -1,4 +1,13 @@
-console.log("hello world")
-if(hour<18){
-    
+//console.log("hello world")
+function printHello(){
+    console.log(userInput.value) 
 }
+
+userInput=document.getElementById("user-input")
+
+let randomnumber=Math.random()
+    randomnumber=randomnumber *(3-0) +0
+    randomnumber=Math.floor(randomnumber)
+    console.log(randomnumber)
+let moves = ["rock","paper","scissors"]
+let computer_moves= moves[randomnumber]
