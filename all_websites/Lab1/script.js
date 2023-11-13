da4bc75818995dd4c0179e769321db2cd117a9c2
Aidan705 <Aidanwilson705@gmail.com>
2023-11-13 16:36:04 -0500

    commit

--- all_websites/Lab1/script.js
+++ all_websites/Lab1/script.js
@@ -2,13 +2,71 @@
 function printHello(){
     console.log(userInput.value) 
 }
+function winner(){
 
-userInput=document.getElementById("user-input")
+    
+    let userInput=document.getElementById("user-input")
+    let winnerTag = document.getElementById("winner")
+    let computer = document.getElementById("computer")
 
-let randomnumber=Math.random()
-    randomnumber=randomnumber *(3-0) +0
-    randomnumber=Math.floor(randomnumber)
-    console.log(randomnumber)
-let moves = ["rock","paper","scissors"]
-let computer_moves= moves[randomnumber]
+    let randomnumber=Math.random()
+        randomnumber=randomnumber *(3-0) +0
+        randomnumber=Math.floor(randomnumber)
+        console.log(randomnumber)
+    let moves = ["computer chose rock","computer chose paper","computer chose scissors"]
+    let computer_moves= moves[randomnumber]
+    
+    console.log("computer move: " + computer_moves)
+    console.log("player moves: " + userInput.value)
+    
+    let winnerText = ''
+
+    if(userInput.value==computer_moves){
+        
+        winnerText = "Its a tie!"
+    }
+    
+    if(userInput.value == "rock" && computer_moves=="computer chose paper"){
+    
+        winnerText = "You lost!"
+    }
+    if(userInput.value == "paper" && computer_moves == "computer chose scissors"){
+    
+        winnerText = "You lost!"
+    }
+    if(userInput.value == "scissors" && computer_moves == "computer chose rock"){
+    
+        winnerText = "You lost!"
+    }
+    if(userInput.value == "rock" && computer_moves == "computer chose scissors"){
+    
+        winnerText = "You won!"
+    }
+    if(userInput.value == "paper" && computer_moves== "computer chose rock"){
+    
+        winnerText = "You won!"
+    }
+    if(userInput.value == "scissors" && computer_moves== "computer chose paper"){
+    
+        winnerText = "You won!"
+    }
+    
+    if(userInput.value == "rock" && computer_moves == "computer chose rock"){
+    
+        winnerText = "Its a tie!"
+    }
+    if(userInput.value == "paper" && computer_moves== "computer chose paper"){
+    
+        winnerText = "Its a tie!"
+    }
+    if(userInput.value == "scissors" && computer_moves== "computer chose scissors"){
+    
+        winnerText = "Its a tie!"
+    }
+    
+    // console.log(winnerText)
+    winnerTag.innerHTML = winnerText
+    computer.innerHTML = computer_moves
+   
+}    
 
